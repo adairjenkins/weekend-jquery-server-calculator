dc9cd83585d69bbe747513308a87871a87138e55
Maggie Jenkins <adair.jenkins@gmail.com>
2022-03-14 15:21:09 -0500

    update port for heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 // make a server
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
